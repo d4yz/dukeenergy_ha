7158115bfa8f72a5fbdcd474e841cca36f89c7a7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-14 22:42:53 +0800

    Delete custom_components directory

--- custom_components/dukeenergy_ha/__init__.py
+++
@@ -1,7 +0,0 @@
-from homeassistant import core
-
-
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the Duke Energy component."""
-    # @TODO: Add setup code.
-    return True
--- custom_components/dukeenergy_ha/const.py
+++
@@ -1,1 +0,0 @@
-DOMAIN = "dukeenergy_ha"
